498d506ff1e337bf7b4ef31f92dc0be6f51e209f
naslanzade <novrastafa@code.edu.az>
2023-02-04 19:10:06 +0400

    add folder

---
+++ script.js
@@ -0,0 +1,98 @@
+"use strict"
+
+//Task-1
+// let number = prompt("Please enter number:");
+
+// if (number == 0) {
+//     console.log("Please enter correct number");
+// }
+// else if (number % 3 == 0 && number % 7 == 0) {
+//     console.log(number);
+// } else {
+//     console.log("write correct number");
+// }
+
+
+//Task-2
+// let num1 = prompt("Please enter first number:");
+// let num2 = prompt("Please enter second number:");
+
+// if (num1 % 2 == 0 && num2 % 2 == 0) {
+//     console.log(Number(num1)+Number(num2));
+// }
+// else {
+//     console.log("Please enter correct number");
+// }
+
+
+// Task-3
+// let n=3;
+// let m=9;
+// let count=0;
+
+// if(n>m)
+// {
+//     console.log("Please write correct range of numbers");
+// }
+// else
+// {
+//     for(let i=n; i<=m;i++){
+//         if(i%2!=0)
+//         {
+//             count++;
+//         }
+        
+//     }
+
+//     console.log(`The count of odd numbers from ${n} - ${m} is: \n ${count}`);
+// }
+
+
+
+//Task-4
+// let n=1;
+// let m=9;
+// let sum=0;
+
+// if(n>m)
+// {
+//     console.log("Please write correct range of numbers");
+// }
+// else
+// {
+//     for(let i=n; i<=m;i++){
+//         if(i%2!=0)
+//         {
+//             sum=sum+i
+//         }
+        
+//     }
+
+//     console.log(`The sum of odd numbers from ${n} - ${m} is: \n ${sum}`);
+// }
+
+
+
+//Task-5
+//  let n=3;
+//  let m=11;
+
+// if(n>m)
+// {
+//     console.log("Please write correct range of numbers");
+// }
+//  else
+//  {
+//     for(let i=n; i<=m;i++){
+//         if(i%2!=0)
+//         {
+//             console.log(i);
+//         }
+        
+//     }     
+//  }
+
+
+
+
+
